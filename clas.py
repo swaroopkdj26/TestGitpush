336e72174afc52d7e884a72fd390cdc02e53c994
test <swaroopkdj26@gmail.com>
2023-06-08 15:16:47 +0530

    This is my First code push in git

---
+++ clas.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self,asjbdsaj,sex,age):
+        self.name =asjbdsaj
+        self.sex = sex
+        self.age = age
+p1 = Person("swrp","male",28)
+
+print(p1.name)
+print(p1.sex)
+print(p1.age)
---
+++ mongotestfile.py
@@ -0,0 +1,18 @@
+import pymongo
+
+
+
+client = pymongo.MongoClient("mongodb+srv://swaroopkdj26:Ignite@cluster0.oeivlla.mongodb.net/?retryWrites=true&w=majority")
+
+db=client.test
+print(db)
+
+d = {
+    "name":"swrp",
+    "email":"sdj26@gmail.com",
+    "surname":"Jadhao"
+}
+
+db1= client['mongotest']
+coll =db1['test']
+coll.insert_one(d )
